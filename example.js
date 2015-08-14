e096a47f979fca43b016473f810ab313711f2a7c
Carlos Soltero <carlosh95@gmail.com>
2015-08-13 21:51:32 -0400

    Simple web server is responding at port 1337

--- example.js
+++ example.js
@@ -3,4 +3,4 @@ http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/pain'});
 	res.end('Hello world\n');
 }).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337');
--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
 <h1>Lesson 1.- Create a node server</h1>
 
 In this chapter we will install node.js.
+We have to create a json  file for all the server info.
+	use 'npm init' to do that and follow the steps
+
 
 
 In this project you will create a simple node servers
